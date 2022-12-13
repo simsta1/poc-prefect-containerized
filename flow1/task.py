d334282289a8012dcf7370d2d00bb9f82a45f526
Simon.Staehli <simon.a.staehli@gmail.com>
2022-12-13 10:27:47 +0100

    task for flow 1

---
+++ flow1/task.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pandas as pd
+
+from prefect import task
+
+
+@task(
+    log_prints=True
+)
+def get_random_integer(low: int, high: int):
+    random_integer = np.random.randint(low=low, high=high)
+    print('Starting with Random Integer:', random_integer)
+    return random_integer
+
+@task
+def process_integer(random_integer: int, multiply_with: int):
+    random_integer = random_integer * multiply_with
+
+    return random_integer
+
+@task(
+    log_prints=True
+)
+def load_integer(random_integer):
+    print('End with Random Integer:', random_integer)
+    
+    return 0
