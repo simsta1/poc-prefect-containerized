8b52eae16d565d72170e8a5aa60f37d8da4280a8
Simon.Staehli <simon.a.staehli@gmail.com>
2022-12-13 16:02:09 +0100

    rename

--- example/flow3/pipeline.py
+++ example/flow3/pipeline.py
@@ -5,9 +5,9 @@ from check_torch_cuda import check_cuda
     
 
 @flow(name='pollinator', log_prints=True)
-def pollinator_pipeline(input = None):
+def cuda_pipeline(input = None):
     check_cuda()
 
 
 if __name__ == '__main__':
-    pollinator_pipeline()
+    cuda_pipeline()
